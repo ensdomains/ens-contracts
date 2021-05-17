7d034d0dd78e5d37ecc5c48c3918502e40185745
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-05-17 19:37:11 +0100

    Delete deps.sol

--- contracts/ethregistrar/deps.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity >=0.8.4;
-
-// This file exists to persuade Truffle to compile contracts we need in tests
-// that aren't referenced anywhere else.
-
-import "../registry/ENSRegistry.sol";
-import "../registry/ENSRegistryWithFallback.sol";
-
