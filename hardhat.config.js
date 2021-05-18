107e714c32e6aca5b00fd6c538c92fcc0ad2fd5a
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-05-18 17:25:48 +0100

    Add travis

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomiclabs/hardhat-truffle5");
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-abi-exporter");
+require("@nomiclabs/hardhat-solhint");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
