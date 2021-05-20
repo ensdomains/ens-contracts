95b3ac6f7c4c2911b1adea4aadb4022bde5a58de
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-05-20 11:05:37 +0100

    Added gas reporter and include artifacts (to have binary)

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomiclabs/hardhat-truffle5");
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-abi-exporter");
 require("@nomiclabs/hardhat-solhint");
+require("hardhat-gas-reporter");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
