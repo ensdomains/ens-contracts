c2df8aadf8dc204bf40dd2d7292c3a40146197e4
Nick Johnson <arachnid@notdot.net>
2021-05-24 13:47:19 +1200

    Make INFTFuseWrapper an interface instead of an abstract contract

--- interfaces/INFTFuseWrapper.sol
+++ interfaces/INFTFuseWrapper.sol
@@ -11,7 +11,7 @@ uint96 constant CANNOT_CREATE_SUBDOMAIN = 32;
 uint96 constant CANNOT_REPLACE_SUBDOMAIN = 64;
 uint96 constant CAN_DO_EVERYTHING = 0;
 
-abstract contract INFTFuseWrapper is IERC1155 {
+interface INFTFuseWrapper is IERC1155 {
     event Wrap(
         bytes32 indexed parentNode,
         string indexed label,
@@ -38,19 +38,19 @@ abstract contract INFTFuseWrapper is IERC1155 {
         string calldata label,
         address wrappedOwner,
         uint96 _fuses
-    ) public virtual;
+    ) external;
 
     function wrapETH2LD(
         string calldata label,
         address wrappedOwner,
         uint96 _fuses
-    ) public virtual;
+    ) external;
 
     function unwrap(
         bytes32 node,
         bytes32 label,
         address owner
-    ) public virtual;
+    ) external;
 
     function setSubnodeRecordAndWrap(
         bytes32 node,
@@ -59,29 +59,29 @@ abstract contract INFTFuseWrapper is IERC1155 {
         address resolver,
         uint64 ttl,
         uint96 _fuses
-    ) public virtual returns (bytes32);
+    ) external returns (bytes32);
 
     function setSubnodeOwner(
         bytes32 node,
         bytes32 label,
         address owner
-    ) public virtual returns (bytes32);
+    ) external returns (bytes32);
 
     function setSubnodeOwnerAndWrap(
         bytes32 node,
         string calldata label,
         address newOwner,
         uint96 _fuses
-    ) public virtual returns (bytes32);
+    ) external returns (bytes32);
 
     function isOwnerOrApproved(bytes32 node, address addr)
-        public
-        virtual
+        external 
+      
         returns (bool);
 
-    function setResolver(bytes32 node, address resolver) public virtual;
+    function setResolver(bytes32 node, address resolver) external;
 
-    function setTTL(bytes32 node, uint64 ttl) public virtual;
+    function setTTL(bytes32 node, uint64 ttl) external;
 }
 
 // events for wrapping names, unwrap, setFuses
