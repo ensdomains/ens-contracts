74b0c4cd0b80119a855dc6f43f9c4423a8554109
Makoto <inouemak@googlemail.com>
2021-05-24 20:14:24 +0100

    Remove Testbuffer

--- test/dnssec-oracle/TestSolidityTests.js
+++ test/dnssec-oracle/TestSolidityTests.js
@@ -1,8 +1,7 @@
 const TestBytesUtils = artifacts.require("./test/dnssec-oracle/TestBytesUtils");
 const TestRRUtils = artifacts.require("./test/dnssec-oracle/TestRRUtils");
-const TestBuffer = artifacts.require("./test/dnssec-oracle/TestBuffer");
 
-[TestBytesUtils, TestRRUtils, TestBuffer].forEach(function(testsuite) {
+[TestBytesUtils, TestRRUtils].forEach(function(testsuite) {
   contract(testsuite.contractName, function(accounts) {
     let instance;
     before(async () => {
