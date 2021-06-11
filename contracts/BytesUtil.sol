7c68c116e3c8b7b325ff2042b13a16788bb395ef
Nick Johnson <arachnid@notdot.net>
2021-06-11 14:17:04 +1200

    Remove readUint96 from bytesutils

--- contracts/BytesUtil.sol
+++ contracts/BytesUtil.sol
@@ -2,27 +2,6 @@ pragma solidity >=0.8.4;
 
 library BytesUtils {
     /*
-     * @dev Returns the 96-bit number at the specified index of self.
-     * @param self The byte string.
-     * @param idx The index into the bytes
-     * @return The specified 96 bits of the string, interpreted as an integer.
-     */
-
-    function readUint96(bytes memory self, uint256 idx)
-        internal
-        pure
-        returns (uint96 ret)
-    {
-        require(idx + 12 <= self.length);
-        assembly {
-            ret := and(
-                mload(add(add(self, 12), idx)),
-                0xFFFFFFFFFFFFFFFFFFFFFFFF
-            )
-        }
-    }
-
-    /*
     * @dev Returns the keccak-256 hash of a byte range.
     * @param self The byte string to hash.
     * @param offset The position to start hashing at.
