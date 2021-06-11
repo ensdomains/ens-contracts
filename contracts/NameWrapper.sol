3fc3bce393a4718e9abced17f53fb232a09e8726
Nick Johnson <arachnid@notdot.net>
2021-06-11 14:20:09 +1200

    Remove redundant interface definition

--- contracts/NameWrapper.sol
+++ contracts/NameWrapper.sol
@@ -18,7 +18,6 @@ contract NameWrapper is Ownable, ERC1155Fuse, INameWrapper, IERC721Receiver {
     IMetadataService public metadataService;
     mapping(bytes32 => bytes) public names;
 
-    //bytes4 private constant ERC721_RECEIVED = 0x150b7a02;
     bytes32 private constant ETH_NODE =
         0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae;
     bytes32 private constant ROOT_NODE =
