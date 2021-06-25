0e62910b3cb07ad5303e268256b405bab63363be
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-06-25 12:22:23 +0100

    Add deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,11 @@
 const { utils } = require('ethers')
 const fs = require('fs')
 const { execSync } = require('child_process');
-
 require('@nomiclabs/hardhat-waffle')
 require('hardhat-gas-reporter')
+require("@nomiclabs/hardhat-etherscan");
+const envfile = require('envfile')
+const { PRIVATE_KEY, ETHERSCAN_API_KEY, INFURA_API_KEY } = envfile.parse(fs.readFileSync('./.env'));
 
 const commit = execSync('git rev-parse --short HEAD', ).toString().trim();
 
@@ -21,6 +23,13 @@ module.exports = {
     localhost: {
       url: 'http://localhost:8545',
     },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
   },
   gasReporter: {
     excludeContracts: ['mocks', 'registry', 'ethregistrar'],
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,56 +2,59 @@ const fs = require('fs')
 const envfile = require('envfile')
 const sourcePath = './env'
 const ensAppSourcePath = '../ens-app/cypress.env.json'
+const { network } = require("hardhat");
+const parsedFile = envfile.parse(fs.readFileSync('./.env'));
 async function main() {
-  const addresses = JSON.parse(fs.readFileSync(ensAppSourcePath, 'utf8'))
-  console.log({addresses})
-
-  if(!(addresses.ensAddress && addresses.baseRegistrarAddress)){
-    throw('please run yarn preTest on ../ens-app')
+  let registryAddress, registrarAddress
+  if(network.name === 'localhost'){
+    const addresses = JSON.parse(fs.readFileSync(ensAppSourcePath, 'utf8'))
+    registryAddress = addresses.ensAddress
+    registrarAddress = addresses.baseRegistrarAddress
+    if(!(addresses.ensAddress && addresses.baseRegistrarAddress)){
+      throw('please run yarn preTest on ../ens-app')
+    }  
+  }else{
+    // Regisry and registrar addresses are same across all networks
+    registryAddress = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
+    registrarAddress = '0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85'
   }
-  const registryAddress = addresses.ensAddress
-  const registrarAddress = addresses.baseRegistrarAddress
-
-
-    const [deployer] = await ethers.getSigners();
-  
-    console.log(
-      "Deploying contracts with the account:",
-      deployer.address
-    );
+  const [deployer] = await ethers.getSigners();
     
-    console.log("Account balance:", (await deployer.getBalance()).toString());
-    const NameWrapper = await ethers.getContractFactory("NameWrapper");
-    console.log({
-      registryAddress, registrarAddress
-    })
-    const StaticMetadataService = await ethers.getContractFactory("StaticMetadataService");
-    console.log(2)
-    const metadata = await StaticMetadataService.deploy(
-      'https://ens.domains'
-    )
-    console.log(3)
-    console.log("StaticMetadataService address:", metadata.address);
-
-    const wrapper = await NameWrapper.deploy(
-      registryAddress,
-      registrarAddress,
-      metadata.address
-    );
-  
-    console.log("Wrapper address:", wrapper.address);
-
-    const parsedFile = envfile.parse(sourcePath);
-    parsedFile.REGISTRY_ADDRESS = registryAddress
-    parsedFile.REGISTRAR_ADDRESS = registrarAddress
-    parsedFile.METADATA_ADDRESS = metadata.address
-    parsedFile.WRAPPER_ADDRESS = wrapper.address
-    fs.writeFileSync('./.env', envfile.stringify(parsedFile))
+  console.log(`Deploying contracts to ${network.name} with the account:${deployer.address}`);
+  const balance = (await deployer.getBalance()).toString()
+  console.log("Account balance:", balance, balance > 0);
+  if(balance === 0){
+    throw(`Not enough eth`)
   }
+
+  const NameWrapper = await ethers.getContractFactory("NameWrapper");
+  console.log({
+    registryAddress, registrarAddress
+  })
+  const StaticMetadataService = await ethers.getContractFactory("StaticMetadataService");
+  const metadata = await StaticMetadataService.deploy(
+    'https://ens-metadata-service.appspot.com/name/0x{id}'
+  )
+  console.log("StaticMetadataService address:", metadata.address);
+  const wrapper = await NameWrapper.deploy(
+    registryAddress,
+    registrarAddress,
+    metadata.address
+  );
+
+  console.log("Wrapper address:", wrapper.address);
+
+  parsedFile.REGISTRY_ADDRESS = registryAddress
+  parsedFile.REGISTRAR_ADDRESS = registrarAddress
+  parsedFile.METADATA_ADDRESS = metadata.address
+  parsedFile.WRAPPER_ADDRESS = wrapper.address
   
-  main()
-    .then(() => process.exit(0))
-    .catch(error => {
-      console.error(error);
-      process.exit(1);
-    });
+  fs.writeFileSync('./.env', envfile.stringify(parsedFile))
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
