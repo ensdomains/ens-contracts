ec16250b14e26b02c7fbbddc0d29f350484d60a0
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-06-29 12:28:46 +0100

    Set domains.ens.nft.image

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,12 +4,14 @@ const sourcePath = './env'
 const ensAppSourcePath = '../ens-app/cypress.env.json'
 const { network } = require("hardhat");
 const parsedFile = envfile.parse(fs.readFileSync('./.env'));
+
 async function main() {
-  let registryAddress, registrarAddress
+  let registryAddress, registrarAddress, metadataUrl
   if(network.name === 'localhost'){
     const addresses = JSON.parse(fs.readFileSync(ensAppSourcePath, 'utf8'))
     registryAddress = addresses.ensAddress
     registrarAddress = addresses.baseRegistrarAddress
+    metadataUrl = 'http://localhost:8080/name/0x{id}'
     if(!(addresses.ensAddress && addresses.baseRegistrarAddress)){
       throw('please run yarn preTest on ../ens-app')
     }  
@@ -17,6 +19,7 @@ async function main() {
     // Regisry and registrar addresses are same across all networks
     registryAddress = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
     registrarAddress = '0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85'
+    metadataUrl = 'https://ens-metadata-service.appspot.com/name/0x{id}'
   }
   const [deployer] = await ethers.getSigners();
     
@@ -32,23 +35,28 @@ async function main() {
     registryAddress, registrarAddress
   })
   const StaticMetadataService = await ethers.getContractFactory("StaticMetadataService");
-  const metadata = await StaticMetadataService.deploy(
-    'https://ens-metadata-service.appspot.com/name/0x{id}'
-  )
+  const metadata = await StaticMetadataService.deploy(metadataUrl)
+  await metadata.deployTransaction.wait()
   console.log("StaticMetadataService address:", metadata.address);
   const wrapper = await NameWrapper.deploy(
     registryAddress,
     registrarAddress,
     metadata.address
   );
-
+  await wrapper.deployTransaction.wait()
   console.log("Wrapper address:", wrapper.address);
-
+    ethers.ContractFactor
+  const PublicResolver = await ethers.getContractFactory("PublicResolver");
+  const metadata = {address:'0xecc5d54d2da9e23caa5c70cda83ecd6ea3be7345'}
+  const wrapper = {address:'0x9029c1574f91696026358d4edB0De773d0E04aeD'}
+  const resolver = await PublicResolver.deploy(registryAddress, wrapper.address)
+  await resolver.deployTransaction.wait()
+  console.log("Resolver address:", resolver.address);
   parsedFile.REGISTRY_ADDRESS = registryAddress
   parsedFile.REGISTRAR_ADDRESS = registrarAddress
   parsedFile.METADATA_ADDRESS = metadata.address
   parsedFile.WRAPPER_ADDRESS = wrapper.address
-  
+  parsedFile.RESOLVER_ADDRESS = resolver.address
   fs.writeFileSync('./.env', envfile.stringify(parsedFile))
 }
 
--- scripts/seed.js
+++ scripts/seed.js
@@ -6,48 +6,51 @@ const packet = require('dns-packet')
 const { utils, BigNumber: BN } = ethers
 const { use, expect } = require('chai')
 const { solidity } = require('ethereum-waffle')
+const parsedFile = envfile.parse(fs.readFileSync('./.env'));
 
 use(solidity)
 
 const namehash = n.hash
 const labelhash = (label) => utils.keccak256(utils.toUtf8Bytes(label))
 
-function encodeName(name) {
-  return '0x' + packet.name.encode(name).toString('hex')
-}
-
-async function main() {
-    const sourceFile = fs.readFileSync(sourcePath)
-    const parsedFile = envfile.parse(sourceFile);
+async function main(a) {
     const [deployer] = await ethers.getSigners();
-    console.log({parsedFile})
     const CAN_DO_EVERYTHING = 0
     const CANNOT_UNWRAP = 1
     const CANNOT_SET_RESOLVER = 8
     const firstAddress = deployer.address
-    console.log("Account balance:", (await deployer.getBalance()).toString());
     const {
       REGISTRY_ADDRESS:registryAddress,
       REGISTRAR_ADDRESS:registrarAddress,
-      WRAPPER_ADDRESS:wrapperAddress
+      WRAPPER_ADDRESS:wrapperAddress,
+      RESOLVER_ADDRESS:resolverAddress,
+      SEED_NAME: name = 'wrappertest'
     } = parsedFile
+    if(!(registryAddress && registrarAddress && wrapperAddress && resolverAddress)){
+      throw('Set addresses on .env')
+    }  
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
     console.log({
-      registryAddress,registrarAddress, wrapperAddress, firstAddress
+      registryAddress,registrarAddress, wrapperAddress, resolverAddress,firstAddress, name
     })
     const EnsRegistry = await (await ethers.getContractFactory("ENSRegistry")).attach(registryAddress);
     const BaseRegistrar = await (await ethers.getContractFactory("BaseRegistrarImplementation")).attach(registrarAddress);
     const NameWrapper = await (await ethers.getContractFactory("NameWrapper")).attach(wrapperAddress);
-    const namehashedname = namehash('postmigration.eth')
-    const labelhashedname = labelhash('postmigration')
-    console.log('postimigration.eth', {namehashedname, labelhashedname})
-    await BaseRegistrar.setApprovalForAll(NameWrapper.address, true)
-    await EnsRegistry.setApprovalForAll(NameWrapper.address, true)
-    await NameWrapper.wrapETH2LD('postmigration', firstAddress, CAN_DO_EVERYTHING)
-    await NameWrapper.setSubnodeOwnerAndWrap(namehash('postmigration.eth'), 'sub1', firstAddress, CAN_DO_EVERYTHING)
-    await NameWrapper.setSubnodeOwnerAndWrap(namehash('postmigration.eth'), 'sub2', firstAddress, CAN_DO_EVERYTHING)
-    await NameWrapper.burnFuses(namehash('sub2.postmigration.eth'),CANNOT_UNWRAP)
-    await NameWrapper.burnFuses(namehash('sub2.postmigration.eth'),CANNOT_SET_RESOLVER)
-    await NameWrapper.unwrap(namehash('postmigration.eth'), labelhash('sub1'), firstAddress)
+    const Resolver = await (await ethers.getContractFactory("PublicResolver")).attach(resolverAddress);
+    const namehashedname = namehash(`${name}.eth`)
+    await (await BaseRegistrar.setApprovalForAll(NameWrapper.address, true)).wait()
+    await (await EnsRegistry.setApprovalForAll(NameWrapper.address, true)).wait()
+    await (await NameWrapper.wrapETH2LD(name, firstAddress, CAN_DO_EVERYTHING)).wait()
+    await (await NameWrapper.setSubnodeOwnerAndWrap(namehash(`${name}.eth`), 'sub1', firstAddress, CAN_DO_EVERYTHING)).wait()
+    await (await NameWrapper.setSubnodeOwnerAndWrap(namehash(`${name}.eth`), 'sub2', firstAddress, CAN_DO_EVERYTHING)).wait()
+    await (await NameWrapper.setResolver(namehash(`sub2.${name}.eth`), resolverAddress)).wait()
+    await (await Resolver.setText(namehash(`sub2.${name}.eth`), 'domains.ens.nft.image', 'https://i.imgur.com/JcZESMp.png')).wait()
+    let text = await Resolver.text(namehash(`sub2.${name}.eth`), 'avatar')
+    console.log({text})
+    await (await NameWrapper.burnFuses(namehash(`sub2.${name}.eth`),CANNOT_UNWRAP)).wait()
+    await (await NameWrapper.burnFuses(namehash(`sub2.${name}.eth`),CANNOT_SET_RESOLVER)).wait()
+    await (await NameWrapper.unwrap(namehash(`${name}.eth`), labelhash('sub1'), firstAddress)).wait()
     let tokenURI = await NameWrapper.uri(namehashedname)
     console.log('owner', {tokenURI})
   }
