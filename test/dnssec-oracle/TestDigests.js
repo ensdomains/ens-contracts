21b9dc0a010de747a261620903fd8160e528e244
Nick Johnson <arachnid@notdot.net>
2021-07-14 10:09:15 +1200

    Add tests

--- test/dnssec-oracle/TestDigests.js
+++ test/dnssec-oracle/TestDigests.js
@@ -1,17 +1,21 @@
 const digests = require('./data/digests');
 
-digests.forEach(function([digest, valid, valid2, invalid]) {
+digests.forEach(function([digest, valids, invalids]) {
   contract(digest, function(accounts) {
     const algorithm = artifacts.require('./digests/' + digest + '.sol');
 
     it('should return true for valid hashes', async function() {
       var instance = await algorithm.deployed();
-      assert.equal(await instance.verify(valid[0], valid[1]), true); // @todo need to convert foo to bytes
+      Promise.all(valids.forEach(async function([text, digest]) {
+        assert.equal(await instance.verify(text, digest), true); // @todo need to convert foo to bytes
+      }));
     });
 
     it('should return false for invalid hashes', async function() {
       var instance = await algorithm.deployed();
-      assert.equal(await instance.verify(invalid[0], invalid[1]), false);
+      Promise.all(invalids.forEach(async function([text, digest]) {
+        assert.equal(await instance.verify(text, digest), false); // @todo need to convert foo to bytes
+      }));
     });
   });
 });
