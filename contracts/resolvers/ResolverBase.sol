2d4b2a7405e1d0d7d4f666f6ab6af1b02122ff42
Nick Johnson <arachnid@notdot.net>
2021-07-22 13:58:53 +1200

    Move bytesToAddress and addressToBytes into addr resolver

--- contracts/resolvers/ResolverBase.sol
+++ contracts/resolvers/ResolverBase.sol
@@ -12,18 +12,4 @@ abstract contract ResolverBase {
         require(isAuthorised(node));
         _;
     }
-
-    function bytesToAddress(bytes memory b) internal pure returns(address payable a) {
-        require(b.length == 20);
-        assembly {
-            a := div(mload(add(b, 32)), exp(256, 12))
-        }
-    }
-
-    function addressToBytes(address a) internal pure returns(bytes memory b) {
-        b = new bytes(20);
-        assembly {
-            mstore(add(b, 32), mul(a, exp(256, 12)))
-        }
-    }
 }
--- contracts/resolvers/profiles/AddrResolver.sol
+++ contracts/resolvers/profiles/AddrResolver.sol
@@ -49,4 +49,18 @@ abstract contract AddrResolver is ResolverBase {
     function supportsInterface(bytes4 interfaceID) virtual override public pure returns(bool) {
         return interfaceID == ADDR_INTERFACE_ID || interfaceID == ADDRESS_INTERFACE_ID || super.supportsInterface(interfaceID);
     }
+
+    function bytesToAddress(bytes memory b) internal pure returns(address payable a) {
+        require(b.length == 20);
+        assembly {
+            a := div(mload(add(b, 32)), exp(256, 12))
+        }
+    }
+
+    function addressToBytes(address a) internal pure returns(bytes memory b) {
+        b = new bytes(20);
+        assembly {
+            mstore(add(b, 32), mul(a, exp(256, 12)))
+        }
+    }
 }
