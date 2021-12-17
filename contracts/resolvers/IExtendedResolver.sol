90020d720b2ce456d08ecb7d951f4fe6e3ec7647
Nick Johnson <arachnid@notdot.net>
2021-12-18 09:25:03 +1300

    Remove supportsInterface from IExtendedResolver

--- contracts/resolvers/IExtendedResolver.sol
+++ contracts/resolvers/IExtendedResolver.sol
@@ -2,6 +2,5 @@
 pragma solidity ^0.8.4;
 
 interface IExtendedResolver {
-    function supportsInterface(bytes4 interfaceID) external pure returns(bool);
     function resolve(bytes memory name, bytes memory data) external view returns(bytes memory);
 }
