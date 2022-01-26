81a70505628d3366bddd17d0f20cfddf1d041ece
Nick Johnson <arachnid@notdot.net>
2022-01-26 15:07:16 +1300

    Write universal resolver; fix interface detection

--- contracts/resolvers/Resolver.sol
+++ contracts/resolvers/Resolver.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
 
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import "./profiles/IABIResolver.sol";
 import "./profiles/IAddressResolver.sol";
 import "./profiles/IAddrResolver.sol";
@@ -11,11 +12,12 @@ import "./profiles/IInterfaceResolver.sol";
 import "./profiles/INameResolver.sol";
 import "./profiles/IPubkeyResolver.sol";
 import "./profiles/ITextResolver.sol";
-import "./ISupportsInterface.sol";
+import "./profiles/IExtendedResolver.sol";
+
 /**
  * A generic resolver interface which includes all the functions including the ones deprecated
  */
-interface Resolver is ISupportsInterface, IABIResolver, IAddressResolver, IAddrResolver, IContentHashResolver, IDNSRecordResolver, IDNSZoneResolver, IInterfaceResolver, INameResolver, IPubkeyResolver, ITextResolver {
+interface Resolver is IERC165, IABIResolver, IAddressResolver, IAddrResolver, IContentHashResolver, IDNSRecordResolver, IDNSZoneResolver, IInterfaceResolver, INameResolver, IPubkeyResolver, ITextResolver, IExtendedResolver {
     /* Deprecated events */
     event ContentChanged(bytes32 indexed node, bytes32 hash);
 
