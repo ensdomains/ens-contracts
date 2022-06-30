1487987e82ad3651530c64ed4db1e36b079bee3d
tate <tate@ens.domains>
2022-06-30 10:38:49 +1000

    added commit/tree hashes to archived deployment file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-solhint'
 import '@nomiclabs/hardhat-truffle5'
 import '@nomiclabs/hardhat-waffle'
+import { exec as _exec } from 'child_process'
 import dotenv from 'dotenv'
 import { existsSync } from 'fs'
 import fs from 'fs/promises'
@@ -9,6 +10,10 @@ import 'hardhat-abi-exporter'
 import 'hardhat-deploy'
 import 'hardhat-gas-reporter'
 import { HardhatUserConfig, task } from 'hardhat/config'
+import { Artifact } from 'hardhat/types'
+import { promisify } from 'util'
+
+const exec = promisify(_exec)
 
 const archivedDeploymentPath = './deployments/archive'
 
@@ -29,10 +34,23 @@ task('accounts', 'Prints the list of accounts', async (_, hre) => {
 task('save', 'Saves a specified contract as a deployed contract')
   .addPositionalParam('contract', 'The contract to save')
   .addPositionalParam('block', 'The block number the contract was deployed at')
+  .addOptionalParam(
+    'fullName',
+    '(Optional) The fully qualified name of the contract (e.g. contracts/resolvers/PublicResolver.sol:PublicResolver)',
+  )
   .setAction(
-    async ({ contract, block }: { contract: string; block: string }, hre) => {
+    async (
+      {
+        contract,
+        block,
+        fullName,
+      }: { contract: string; block: string; fullName?: string },
+      hre,
+    ) => {
       const network = hre.network.name
-      const artifact = await hre.deployments.getArtifact(contract)
+
+      const artifactReference = fullName || contract
+      const artifact = await hre.deployments.getArtifact(artifactReference)
 
       const archiveName = `${contract}_${network}_${block}`
       const archivePath = `${archivedDeploymentPath}/${archiveName}.sol`
@@ -41,14 +59,23 @@ task('save', 'Saves a specified contract as a deployed contract')
         throw new Error('Archive already exists')
       }
 
-      let newArtifact = artifact
-      newArtifact.contractName = archiveName
-      newArtifact.sourceName = archivePath.substring(2)
+      const newArtifact: Artifact & {
+        commitHash: string
+        treeHash: string
+      } = {
+        ...artifact,
+        contractName: archiveName,
+        sourceName: archivePath.substring(2),
+        commitHash: (await exec('git rev-parse HEAD')).stdout.trim(),
+        treeHash: (
+          await exec(`git rev-parse HEAD:${artifact.sourceName}`)
+        ).stdout.trim(),
+      }
 
       await fs.mkdir(archivePath)
       await fs.writeFile(
         `${archivePath}/${archiveName}.json`,
-        JSON.stringify(artifact, null, 2),
+        JSON.stringify(newArtifact, null, 2),
       )
       console.log("Archived contract to '" + archivePath + "'")
     },
