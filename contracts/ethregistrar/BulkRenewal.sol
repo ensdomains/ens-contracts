b3e961a8a53bd878a1a8454456785ffb74c3e54c
Jeff Lau <jeff-lau@live.com>
2022-09-13 18:06:51 +0800

    BulkRenewal - change refund, gas optimisations

--- contracts/ethregistrar/BulkRenewal.sol
+++ contracts/ethregistrar/BulkRenewal.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.8.4;
+pragma solidity >=0.8.15;
 pragma experimental ABIEncoderV2;
 
 import "../registry/ENS.sol";
@@ -38,12 +38,16 @@ contract BulkRenewal is IBulkRenewal {
         returns (uint256 total)
     {
         ETHRegistrarController controller = getController();
-        for (uint256 i = 0; i < names.length; i++) {
+        uint256 length = names.length;
+        for (uint256 i = 0; i < length; ) {
             IPriceOracle.Price memory price = controller.rentPrice(
                 names[i],
                 duration
             );
-            total += (price.base + price.premium);
+            unchecked {
+                ++i;
+                total += (price.base + price.premium);
+            }
         }
     }
 
@@ -54,21 +58,21 @@ contract BulkRenewal is IBulkRenewal {
     {
         ETHRegistrarController controller = getController();
         uint256 length = names.length;
+        uint256 total;
         for (uint256 i = 0; i < length; ) {
             IPriceOracle.Price memory price = controller.rentPrice(
                 names[i],
                 duration
             );
-            controller.renew{value: price.base + price.premium}(
-                names[i],
-                duration
-            );
+            uint256 totalPrice = price.base + price.premium;
+            controller.renew{value: totalPrice}(names[i], duration);
             unchecked {
                 ++i;
+                total += totalPrice;
             }
         }
         // Send any excess funds back
-        payable(msg.sender).transfer(address(this).balance);
+        payable(msg.sender).transfer(msg.value - total);
     }
 
     function supportsInterface(bytes4 interfaceID)
