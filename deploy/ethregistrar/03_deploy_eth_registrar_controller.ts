28f4a13754dab00ca30e64d52d4acb7a616ae02a
Nick Johnson <arachnid@notdot.net>
2022-09-21 14:14:39 +1200

    Don't set ETHRegistrarController as a controller of BaseRegistrar

--- deploy/ethregistrar/03_deploy_eth_registrar_controller.ts
+++ deploy/ethregistrar/03_deploy_eth_registrar_controller.ts
@@ -37,29 +37,21 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   // Only attempt to make controller etc changes directly on testnets
   if(network.name === 'mainnet') return;
 
-  const tx1 = await registrar.addController(controller.address, {
+  const tx1 = await nameWrapper.setController(controller.address, {
     from: deployer,
   })
   console.log(
-    `Adding ETHRegistrarController as controller on BaseRegistrarImplementation (tx: ${tx1.hash})...`,
+    `Adding ETHRegistrarController as a controller of NameWrapper (tx: ${tx1.hash})...`,
   )
   await tx1.wait()
 
-  const tx2 = await nameWrapper.setController(controller.address, {
+  const tx2 = await reverseRegistrar.setController(controller.address, {
     from: deployer,
   })
   console.log(
-    `Adding ETHRegistrarController as a controller of NameWrapper (tx: ${tx2.hash})...`,
+    `Adding ETHRegistrarController as a controller of ReverseRegistrar (tx: ${tx2.hash})...`,
   )
   await tx2.wait()
-
-  const tx3 = await reverseRegistrar.setController(controller.address, {
-    from: deployer,
-  })
-  console.log(
-    `Adding ETHRegistrarController as a controller of ReverseRegistrar (tx: ${tx3.hash})...`,
-  )
-  await tx3.wait()
 
   const artifact = await deployments.getArtifact("IETHRegistrarController");
   const interfaceId = computeInterfaceId(new Interface(artifact.abi));
@@ -70,11 +62,11 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     return;
   }
   const resolverContract = await ethers.getContractAt('PublicResolver', resolver.address);
-  const tx4 = await resolverContract.setInterface(ethers.utils.namehash('eth'), interfaceId, controller.address);
+  const tx3 = await resolverContract.setInterface(ethers.utils.namehash('eth'), interfaceId, controller.address);
   console.log(
-    `Setting ETHRegistrarController interface ID ${interfaceId} on .eth resolver (tx: ${tx4.hash})...`
+    `Setting ETHRegistrarController interface ID ${interfaceId} on .eth resolver (tx: ${tx3.hash})...`
   )
-  await tx4.wait()
+  await tx3.wait()
 }
 
 func.tags = ['ethregistrar', 'ETHRegistrarController']
