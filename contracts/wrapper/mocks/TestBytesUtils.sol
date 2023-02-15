bce29b60c66079dd44d03c073f0189a2124d0088
tate <tate@ens.domains>
2023-02-15 16:40:24 +1100

    add testunwrap contract

---
+++ contracts/wrapper/mocks/TestBytesUtils.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.8.4;
+
+library TestBytesUtils {
+    function memcpy(uint256 dest, uint256 src, uint256 len) private pure {
+        // Copy word-length chunks while possible
+        for (; len >= 32; len -= 32) {
+            assembly {
+                mstore(dest, mload(src))
+            }
+            dest += 32;
+            src += 32;
+        }
+
+        // Copy remaining bytes
+        unchecked {
+            uint256 mask = (256 ** (32 - len)) - 1;
+            assembly {
+                let srcpart := and(mload(src), not(mask))
+                let destpart := and(mload(dest), mask)
+                mstore(dest, or(destpart, srcpart))
+            }
+        }
+    }
+
+    /*
+     * @dev Copies a substring into a new byte string.
+     * @param self The byte string to copy from.
+     * @param offset The offset to start copying at.
+     * @param len The number of bytes to copy.
+     */
+    function substring(
+        bytes memory self,
+        uint256 offset,
+        uint256 len
+    ) internal pure returns (bytes memory) {
+        require(offset + len <= self.length);
+
+        bytes memory ret = new bytes(len);
+        uint256 dest;
+        uint256 src;
+
+        assembly {
+            dest := add(ret, 32)
+            src := add(add(self, 32), offset)
+        }
+        memcpy(dest, src, len);
+
+        return ret;
+    }
+}
