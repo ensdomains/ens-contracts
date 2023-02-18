5553436dad5f66a740ce4e2f77e279945509d7a2
nxt3d <pm@premmakeig.com>
2023-02-18 15:33:20 -0500

    updated Resolver.sol to include approvals

--- contracts/resolvers/Resolver.sol
+++ contracts/resolvers/Resolver.sol
@@ -34,6 +34,14 @@ interface Resolver is
     /* Deprecated events */
     event ContentChanged(bytes32 indexed node, bytes32 hash);
 
+    function setApprovalForAll(address, bool) external; 
+
+    function approve(bytes32 node, address delegate, bool approved) external; 
+
+    function isApprovedForAll(address account, address operator) external;
+
+    function isApprovedFor(address owner, bytes32 node, address delegate) external;
+
     function setABI(
         bytes32 node,
         uint256 contentType,
