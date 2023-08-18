8bd02e6643b33eb92f13e6fc35fd9863ec107747
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2023-08-18 13:24:37 +0200

    add deploy script for ExtendedDNSResolver

---
+++ deploy/resolvers/00_deploy_extended_dns_resolver.ts
@@ -0,0 +1,20 @@
+import { ethers } from 'hardhat'
+import { DeployFunction } from 'hardhat-deploy/types'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { getNamedAccounts, deployments } = hre
+  const { deploy } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  await deploy('ExtendedDNSResolver', {
+    from: deployer,
+    args: [],
+    log: true,
+  })
+}
+
+func.id = 'extended-dns-resolver'
+func.tags = ['resolvers', 'ExtendedDNSResolver']
+
+export default func
