82f15196deb151c41b980a30b49db8e684e97348
Subash Chandra Sapkota <scs@logpoint.com>
2023-09-29 22:47:34 +0545

    chore: add license comment to Solidity file

--- contracts/resolvers/OwnedResolver.sol
+++ contracts/resolvers/OwnedResolver.sol
@@ -1,4 +1,6 @@
+//SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./profiles/ABIResolver.sol";
 import "./profiles/AddrResolver.sol";
