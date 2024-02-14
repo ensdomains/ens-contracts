d73b7d13305498ecf276b6accf335d9dbed6881c
Makoto Inoue <2630+makoto@users.noreply.github.com>
2024-02-14 18:49:21 +0000

    Remove unused code

--- contracts/reverseRegistrar/DefaultReverseResolver.sol
+++ contracts/reverseRegistrar/DefaultReverseResolver.sol
@@ -3,16 +3,10 @@ pragma solidity >=0.8.4;
 import "./IDefaultReverseResolver.sol";
 import "./SignatureReverseResolver.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
-import "../resolvers/profiles/ITextResolver.sol";
-import "../resolvers/profiles/INameResolver.sol";
 import "../../contracts/resolvers/profiles/IExtendedResolver.sol";
 import "../wrapper/BytesUtils.sol";
-import "../utils/HexUtils.sol";
-import "../utils/LowLevelCallUtils.sol";
-import "hardhat/console.sol";
 
 contract DefaultReverseResolver is
     Ownable,
@@ -26,10 +20,6 @@ contract DefaultReverseResolver is
     // The namehash of 'default.reverse'
     bytes32 private constant DEFAULT_REVERSE_NODE =
         0x53a2e7cce84726721578c676b4798972d354dd7c62c832415371716693edd312;
-    // This is the hex encoding of the string 'abcdefghijklmnopqrstuvwxyz'
-    // It is used as a constant to lookup the characters of the hex address
-    bytes32 constant lookup =
-        0x3031323334353637383961626364656600000000000000000000000000000000;
 
     /**
      * @dev Constructor
@@ -70,9 +60,6 @@ contract DefaultReverseResolver is
         if (selector == IDefaultReverseResolver.name.selector) {
             address addr = abi.decode(data[4:], (address));
             string memory n = name(addr);
-            console.logString("1");
-            console.logString(n);
-            console.logBytes(bytes(n));
             return bytes(n);
         }
         if (selector == IDefaultReverseResolver.text.selector) {
