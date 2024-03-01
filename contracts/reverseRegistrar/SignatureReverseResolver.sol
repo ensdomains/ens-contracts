ad32e16375ffec2e4fb4ce975b83087f159af57a
Makoto Inoue <2630+makoto@users.noreply.github.com>
2024-03-01 16:50:18 +0000

    Update contracts/reverseRegistrar/SignatureReverseResolver.sol
    Co-authored-by: Nick Johnson <arachnid@notdot.net>

--- contracts/reverseRegistrar/SignatureReverseResolver.sol
+++ contracts/reverseRegistrar/SignatureReverseResolver.sol
@@ -54,7 +54,7 @@ contract SignatureReverseResolver is Ownable, ISignatureReverseResolver {
     }
 
     function isAuthorised(address addr) internal view virtual returns (bool) {
-        revert("This function needs to be overriten");
+        revert("This function needs to be overridden");
     }
 
     function isAuthorisedWithSignature(
