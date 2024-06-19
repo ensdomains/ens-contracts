36c704da1552bf36602ee7518dc45c65b3ef6724
tate <tate@ens.domains>
2024-06-20 09:44:02 +1000

    testtestregistrar

--- test/test-utils/reverse.js
+++
@@ -1,8 +0,0 @@
-const { namehash } = require('viem/ens')
-
-function getReverseNode(addr) {
-  return namehash(addr.slice(2).toLowerCase() + '.addr.reverse')
-}
-module.exports = {
-  getReverseNode,
-}
