2743709fc209689455725765cbedce7419b8c24a
Andrew Raffensperger <raffy@me.com>
2025-05-08 12:04:55 -0700

    isolate HexUtils/ENSIP19.sol change from feature/default-reverse-resolver

--- contracts/utils/ENSIP19.sol
+++ contracts/utils/ENSIP19.sol
@@ -2,9 +2,15 @@
 pragma solidity ^0.8.0;
 
 import {HexUtils} from "../utils/HexUtils.sol";
+import {NameCoder} from "../utils/NameCoder.sol";
 
+uint32 constant CHAIN_ID_ETH = 1;
 uint256 constant COIN_TYPE_ETH = 60;
 uint256 constant EVM_BIT = 1 << 31;
+
+string constant SLUG_ETH = "addr"; // <=> COIN_TYPE_ETH
+string constant SLUG_DEFAULT = "default"; // <=> EVM_BIT
+string constant TLD_REVERSE = "reverse";
 
 /// @dev Library for generating reverse names according to ENSIP-19.
 /// https://docs.ens.domains/ensip/19
@@ -18,13 +24,22 @@ library ENSIP19 {
     function chainFromCoinType(
         uint256 coinType
     ) internal pure returns (uint32 chain) {
-        if (coinType == COIN_TYPE_ETH) return 1;
+        if (coinType == COIN_TYPE_ETH) return CHAIN_ID_ETH;
         return
             uint32(
                 uint32(coinType) == coinType && (coinType & EVM_BIT) != 0
                     ? coinType ^ EVM_BIT
                     : 0
             );
+    }
+
+    /// @dev Determine if Coin Type is for an EVM address.
+    /// @param coinType The coin type.
+    /// @return isEVM True if coin type represents an EVM address.
+    function isEVMCoinType(
+        uint256 coinType
+    ) internal pure returns (bool isEVM) {
+        isEVM = chainFromCoinType(coinType) != 0 || coinType == EVM_BIT;
     }
 
     /// @dev Same as `reverseName()` but uses EVM Address + Chain ID.
@@ -35,7 +50,7 @@ library ENSIP19 {
         return
             reverseName(
                 abi.encodePacked(addr),
-                chain == 1 ? COIN_TYPE_ETH : chain | EVM_BIT
+                chain == CHAIN_ID_ETH ? COIN_TYPE_ETH : chain | EVM_BIT
             );
     }
 
@@ -52,14 +67,52 @@ library ENSIP19 {
         name = string(
             abi.encodePacked(
                 HexUtils.bytesToHex(encodedAddress),
-                ".",
+                bytes1("."),
                 coinType == COIN_TYPE_ETH
-                    ? "addr"
+                    ? SLUG_ETH
                     : coinType == EVM_BIT
-                        ? "default"
+                        ? SLUG_DEFAULT
                         : HexUtils.unpaddedUintToHex(coinType, true),
-                ".reverse"
+                bytes1("."),
+                TLD_REVERSE
             )
         );
+    }
+
+    /// @dev Parse Reverse Name into Encoded Address + Coin Type.
+    ///      Matches: /^[0-9a-fA-F]+\.([0-9a-f]{1,64}|addr|default)\.reverse$/.
+    ///      Reverts `DNSDecodingFailed`.
+    /// @param name The DNS-encoded name.
+    /// @return encodedAddress The address or empty if invalid.
+    /// @return coinType The coin type.
+    function parse(
+        bytes memory name
+    ) internal pure returns (bytes memory encodedAddress, uint256 coinType) {
+        (, uint256 offset) = NameCoder.readLabel(name, 0);
+        bool valid;
+        (encodedAddress, valid) = HexUtils.hexToBytes(name, 1, offset);
+        if (!valid) return ("", 0); // encodedAddress not hex
+        (bytes32 labelHash, uint256 offset2) = NameCoder.readLabel(
+            name,
+            offset
+        );
+        if (labelHash == keccak256(bytes(SLUG_ETH))) {
+            coinType = COIN_TYPE_ETH;
+        } else if (labelHash == keccak256(bytes(SLUG_DEFAULT))) {
+            coinType = EVM_BIT;
+        } else {
+            bytes32 word;
+            (word, valid) = HexUtils.hexStringToBytes32(
+                name,
+                1 + offset,
+                offset2
+            );
+            if (!valid) return ("", 0); // invalid coinType
+            coinType = uint256(word);
+        }
+        (labelHash, offset) = NameCoder.readLabel(name, offset2);
+        if (labelHash != keccak256(bytes(TLD_REVERSE))) return ("", 0); // invalid tld
+        (labelHash, ) = NameCoder.readLabel(name, offset);
+        if (labelHash != bytes32(0)) return ("", 0); // not tld
     }
 }
--- test/utils/TestENSIP19.ts
+++ test/utils/TestENSIP19.ts
@@ -6,12 +6,20 @@ import {
   COIN_TYPE_ETH,
   EVM_BIT,
   getReverseName,
+  isEVMCoinType,
   shortCoin,
 } from '../fixtures/ensip19.js'
+import { dnsEncodeName } from '../fixtures/dnsEncodeName.js'
 
 async function fixture() {
-  return hre.viem.deployContract('TestENSIP19', [])
+  return hre.viem.deployContract('TestENSIP19')
 }
+
+const addrs = [
+  '0x81',
+  '0x8000000000000000000000000000000000000001',
+  '0x800000000000000000000000000000000000000000000000000000000000000001', // 33 bytes
+] as const
 
 const coinTypes = [COIN_TYPE_ETH, EVM_BIT, 0n, 1n]
 
@@ -24,11 +32,7 @@ describe('ENSIP19', () => {
         .toBeRevertedWithCustomError('EmptyAddress')
     })
 
-    for (const addr of [
-      '0x81',
-      '0x8000000000000000000000000000000000000001',
-      '0x800000000000000000000000000000000000000000000000000000000000000001', // 33 bytes
-    ] as const) {
+    for (const addr of addrs) {
       it(addr, async () => {
         const F = await loadFixture(fixture)
         for (const coinType of coinTypes) {
@@ -41,6 +45,33 @@ describe('ENSIP19', () => {
     }
   })
 
+  describe('parse()', () => {
+    for (const addr of addrs) {
+      it(addr, async () => {
+        const F = await loadFixture(fixture)
+        for (const coinType of coinTypes) {
+          await expect(
+            F.read.parse([dnsEncodeName(getReverseName(addr, coinType))]),
+            shortCoin(coinType),
+          ).resolves.toStrictEqual([addr, coinType])
+        }
+      })
+    }
+
+    for (const name of [
+      'zzz.addr.reverse',
+      '.default.reverse',
+      'abc.reverse',
+    ]) {
+      it(name, async () => {
+        const F = await loadFixture(fixture)
+        await expect(
+          F.read.parse([dnsEncodeName(name)]),
+        ).resolves.toStrictEqual(['0x', 0n])
+      })
+    }
+  })
+
   describe('chainFromCoinType()', () => {
     for (const coinType of coinTypes) {
       it(shortCoin(coinType), async () => {
@@ -48,6 +79,17 @@ describe('ENSIP19', () => {
         await expect(
           F.read.chainFromCoinType([coinType]),
         ).resolves.toStrictEqual(chainFromCoinType(coinType))
+      })
+    }
+  })
+
+  describe('isEVMCoinType()', () => {
+    for (const coinType of coinTypes) {
+      it(shortCoin(coinType), async () => {
+        const F = await loadFixture(fixture)
+        await expect(F.read.isEVMCoinType([coinType])).resolves.toStrictEqual(
+          isEVMCoinType(coinType),
+        )
       })
     }
   })
