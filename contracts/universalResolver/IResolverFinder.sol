e48da4a9ea022f5c1729c32eb4b3661932432cd6
Andrew Raffensperger <raffy@me.com>
2025-06-05 12:12:41 -0400

    forgot to delete interface

--- contracts/universalResolver/IResolverFinder.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IResolverFinder {
-    function findResolver(
-        bytes memory name
-    ) external view returns (address resolver, bytes32 node, uint256 offset);
-}
