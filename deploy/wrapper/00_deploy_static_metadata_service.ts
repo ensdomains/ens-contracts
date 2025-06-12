6276903a63181e54dd3d8964bf84178eb0d5d0bb
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-06-12 13:59:56 -0400

    Update URL format

--- deploy/wrapper/00_deploy_static_metadata_service.ts
+++ deploy/wrapper/00_deploy_static_metadata_service.ts
@@ -4,13 +4,14 @@ const func: DeployFunction = async function (hre) {
   const { network, viem } = hre
 
   let metadataHost =
-    process.env.METADATA_HOST || 'ens-metadata-service.appspot.com'
+    process.env.METADATA_HOST ||
+    'https://metadata.ens.domains/mainnet/0xD4416b13d2b3a9aBae7AcD5D6C2BbDBE25686401'
 
   if (network.name === 'localhost') {
     metadataHost = 'http://localhost:8080'
   }
 
-  const metadataUrl = `${metadataHost}/name/{id}`
+  const metadataUrl = `${metadataHost}/{id}`
 
   await viem.deploy('StaticMetadataService', [metadataUrl])
 }
