c3128420150dde44a029b057a5ac7021356ca220
Andrew Raffensperger <raffy@me.com>
2025-06-30 18:29:30 -0400

    restore mainnet.ts

--- test/universalResolver/mainnet.ts
+++ test/universalResolver/mainnet.ts
@@ -1,4 +1,4 @@
-import { type Address, encodeErrorResult, parseAbi} from 'viem'
+import type { Address } from 'viem'
 import type { KnownProfile, KnownReverse } from '../utils/resolutions.js'
 import { COIN_TYPE_ETH } from '../fixtures/ensip19.js'
 
@@ -19,10 +19,7 @@ export const KNOWN_RESOLUTIONS: KnownProfile[] = [
     errors: [
       {
         call: '0x12345678',
-        answer: encodeErrorResult({
-          abi: parseAbi(['error UnsupportedResolverProfile(bytes4)']),
-          args: ['0x12345678'],
-        }),
+        answer: '0x',
       },
     ],
   },
