d30520cf6e2daf39ced6def11ff9ef83afb75abc
Andrew Raffensperger <raffy@me.com>
2025-07-25 11:17:40 -0700

    update comment

--- contracts/dnssec-oracle/IDNSGateway.sol
+++ contracts/dnssec-oracle/IDNSGateway.sol
@@ -4,11 +4,13 @@ pragma solidity ^0.8.0;
 import {DNSSEC} from "./DNSSEC.sol";
 
 /// @notice Interface for the offchain DNSSEC oracle gateway.
+///         https://docs.ens.domains/ensip/17#dnssec-gateway-api
 /// @dev Interface selector: `0x31b137b9`
 interface IDNSGateway {
+    /// @dev Fetch verifiable DNSSEC resource records of a specific type for a name.
     /// @param name The DNS-encoded name.
-    /// @param qtype The DNS record query type.
-    /// @return The list of verifiable DNS resource records.
+    /// @param qtype The DNS record query type according to RFC-1034.
+    /// @return The list of verifiable DNS resource records according to RFC-4035.
     function resolve(
         bytes memory name,
         uint16 qtype
