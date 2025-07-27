6ebadea49cdbb67bc3a31a7b6c4130626fa241f2
Andrew Raffensperger <raffy@me.com>
2025-07-27 15:17:36 -0700

    fix TestUniversalResolver.remote.ts

--- test/universalResolver/TestUniversalResolver.remote.ts
+++ test/universalResolver/TestUniversalResolver.remote.ts
@@ -13,9 +13,13 @@ import { bundleCalls, makeResolutions } from '../utils/resolutions.js'
 async function fixture() {
   const bg = await serveBatchGateway()
   after(bg.shutdown)
+  const batchGatewayProvider = await hre.viem.deployContract(
+    'GatewayProvider',
+    [[bg.localBatchGatewayUrl]],
+  )
   return hre.viem.deployContract(
     'UniversalResolver',
-    [ENS_REGISTRY, [bg.localBatchGatewayUrl]],
+    [ENS_REGISTRY, batchGatewayProvider.address],
     {
       client: {
         public: await hre.viem.getPublicClient({ ccipRead: undefined }),
