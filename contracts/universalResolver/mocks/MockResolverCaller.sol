05e2b9a80b9bbb926c09f13bc509428888e3bb8d
Andrew Raffensperger <raffy@me.com>
2025-08-21 23:37:43 -0400

    remove unused headers

--- contracts/universalResolver/mocks/MockResolverCaller.sol
+++ contracts/universalResolver/mocks/MockResolverCaller.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import {CCIPReader} from "../../ccipRead/CCIPReader.sol";
 import {ResolverCaller} from "../ResolverCaller.sol";
-import {IExtendedResolver} from "../../resolvers/profiles/IExtendedResolver.sol";
 
 contract MockResolverCaller is ResolverCaller {
     constructor() CCIPReader(0) {}
